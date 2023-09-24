b57d7b10c4eba43d077d600460feb1ec5de34615
LeTriThanh2021603411 <144913112+LeTriThanh2021603411@users.noreply.github.com>
2023-09-24 23:09:20 +0700

    Thêm_thanh_trượt_hiển_thị_kq_khi_kích_thước_ma_trận_lớn.py
    Kích cỡ ma trận đến 20

--- matrix-calculator-main/trans.py
+++ matrix-calculator-main/trans.py
@@ -1,5 +1,5 @@
-from tkinter import Label, Button, Entry, OptionMenu, IntVar, StringVar, Frame, Toplevel, messagebox
-from tkinter.constants import BOTH
+from tkinter import Label, Button, Entry, OptionMenu, IntVar, StringVar, Frame, Toplevel, messagebox, Canvas, Scrollbar
+from tkinter.constants import BOTH, VERTICAL, HORIZONTAL
 import numpy as np
 import menu
 
@@ -17,45 +17,66 @@ class Trans:
             return self.trans_matrix
         except:
             pass
-        #     list_mat = [str(i) for i in np.transpose(self.matrix)]
-
-        #     # remove square brackets
-        #     for i in range(len(list_mat)):
-        #         list_mat[i] = list_mat[i][1:-1]
-        #     return list_mat
-
-        # except (TypeError, Exception):
-        #     Label(self.frame_trans_output, text="Ma trận của bạn").grid(row=1, column=self.cols_get * 2 + 10)
-        #     Label(self.frame_trans_output, text="Không hợp lệ!").grid(row=2, column=self.cols_get * 2 + 10)
 
     def output_matrix(self):
         self.gui_trans_input.destroy()
         self.gui_trans_output = Toplevel()
-        self.gui_trans_output.title("Transpose")
+        self.gui_trans_output.title("Chuyển vị")
         self.gui_trans_output.resizable(False, False)
 
         self.frame_trans_output = Frame(self.gui_trans_output, highlightbackground='black', highlightthickness=1, padx=5, pady=5)
         self.frame_trans_output.pack(fill=BOTH, expand=True, padx=5, pady=5)
 
-         #button
+        # button
         self.frame_button = Frame(self.gui_trans_output, bg='#F9E79F', highlightbackground='black', highlightthickness=1, padx=5, pady=5)
         self.frame_button.pack(fill='x', expand=True, padx=5, pady=3)
         Button(self.frame_button, text="Back", font=('arial', 10, 'bold'), width=11, activebackground='green', command=self.back_to_input).grid(row=0, column=1, sticky='e', padx=14)
         Button(self.frame_button, text="Back to Menu", font=('arial', 10, 'bold'), activebackground='green', command=self.back_to_menu_from_output).grid(row=0, column=0, sticky='e', padx=14)
         Button(self.frame_button, text="Exit", font=('arial', 10, 'bold'), width=11, activebackground='green', command=exit).grid(row=0, column=2, sticky='e', padx=14)
 
-        Label(self.frame_trans_output, text='Input:', font=('arial', 14, 'bold'), underline=0).grid(row=0, column=0, sticky='W')
+        Label(self.frame_trans_output, text='Ma trận đã nhập:', font=('arial', 14, 'bold'), underline=0).grid(row=0, column=0, sticky='W')
+
+        ############
+        canvas_input = Canvas(self.frame_trans_output, width=700, height=200)
+        canvas_input.grid(row=1, column=0, padx=5, pady=5, columnspan=self.cols_get + 1)
+        v_scrollbar_input = Scrollbar(self.frame_trans_output, orient=VERTICAL, command=canvas_input.yview)
+        v_scrollbar_input.grid(row=1, column=self.cols_get + 2, sticky='ns')
+        h_scrollbar_input = Scrollbar(self.frame_trans_output, orient=HORIZONTAL, command=canvas_input.xview)
+        h_scrollbar_input.grid(row=self.rows_get + 1, column=0, sticky='ew')
+        canvas_input.configure(yscrollcommand=v_scrollbar_input.set, xscrollcommand=h_scrollbar_input.set)
+
+        frame_matrix_input = Frame(canvas_input)
+        canvas_input.create_window((0, 0), window=frame_matrix_input, anchor='nw')
+
         for i in range(self.rows_get):
             for j in range(self.cols_get):
-                Label(self.frame_trans_output, text=self.matrix[i][j], font=('arial', 12, 'bold'), bd=5).grid(row=i + 1, column=j + 1, padx=5, pady=5, sticky='news')
+                Label(frame_matrix_input, text=self.matrix[i][j], font=('arial', 12, 'bold'), bd=5).grid(row=i, column=j, padx=5, pady=5, sticky='news')
 
-        # display output
-        Label(self.frame_trans_output, text='Transposed:', font=('arial', 14, 'bold'), underline=0).grid(row=self.rows_get + 1, column=0, sticky='W')
+        frame_matrix_input.update_idletasks()
+        canvas_input.config(scrollregion=canvas_input.bbox("all"))
+
+        Label(self.frame_trans_output, text='Ma trận cần tìm:', font=('arial', 14, 'bold'), underline=0).grid(row=self.rows_get + 6, column=0, sticky='W')
 
         self.transposed_matrix = self.compute_transpose()
+
+        ################
+        canvas_output = Canvas(self.frame_trans_output, width=400, height=200)
+        canvas_output.grid(row=self.rows_get + 3, column=0, padx=5, pady=5, columnspan=self.cols_get + 1)
+        v_scrollbar_output = Scrollbar(self.frame_trans_output, orient=VERTICAL, command=canvas_output.yview)
+        v_scrollbar_output.grid(row=self.rows_get + 3, column=self.cols_get + 2, sticky='ns')
+        h_scrollbar_output = Scrollbar(self.frame_trans_output, orient=HORIZONTAL, command=canvas_output.xview)
+        h_scrollbar_output.grid(row=self.rows_get + 2, column=0, sticky='ew')
+        canvas_output.configure(yscrollcommand=v_scrollbar_output.set, xscrollcommand=h_scrollbar_output.set)
+
+        frame_matrix_output = Frame(canvas_output)
+        canvas_output.create_window((0, 0), window=frame_matrix_output, anchor='nw')
+
         for i in range(self.cols_get):
             for j in range(self.rows_get):
-                Label(self.frame_trans_output, text=self.transposed_matrix[i][j], font=('arial', 12, 'bold'), bd=5).grid(row=i + self.rows_get + 2, column= j + 1, padx=5, pady=5, sticky='news')
+                Label(frame_matrix_output, text=self.transposed_matrix[i][j], font=('arial', 12, 'bold'), bd=5).grid(row=i, column=j, padx=5, pady=5, sticky='news')
+
+        frame_matrix_output.update_idletasks()
+        canvas_output.config(scrollregion=canvas_output.bbox("all"))
 
         self.gui_trans_output.protocol("WM_DELETE_WINDOW", menu.gui_menu.destroy)
         self.gui_trans_output.mainloop()
@@ -67,13 +88,13 @@ class Trans:
     def input_matrix(self):
         self.gui_trans_menu.destroy()
         self.gui_trans_input = Toplevel()
-        self.gui_trans_input.title("Transpose")
+        self.gui_trans_input.title("Chuyển vị")
         self.gui_trans_input.resizable(False, False)
 
         self.frame_trans_input = Frame(self.gui_trans_input, highlightbackground='black', highlightthickness=1, bg='#F9E79F', padx=5, pady=5)
         self.frame_trans_input.pack(fill=BOTH, expand=True, padx=5, pady=5)
 
-        Label(self.frame_trans_input, text="Matrix:", font=('arial', 12, 'bold'), bg='#F9E79F').grid(row=0, column=0)
+        Label(self.frame_trans_input, text="Ma trận:", font=('arial', 12, 'bold'), bg='#F9E79F').grid(row=0, column=0)
 
         # empty arrays for Entry and StringVars
         text_var = []
@@ -92,7 +113,7 @@ class Trans:
                 # append StringVar
                 text_var[i].append(StringVar())
 
-                # append the entry into the list
+                # append the entry into the list5
                 entries[i].append(Entry(self.frame_trans_input, textvariable=text_var[i][j], width=10, font=('arial', 10, 'bold')))
 
                 # display entry
@@ -112,11 +133,11 @@ class Trans:
             except (ValueError, Exception):
                 messagebox.showerror('Error', 'Ma trận không hợp lệ')
 
-        self.frame_button = Frame(self.gui_trans_input,  bg='#F9E79F', highlightbackground='black', highlightthickness=1, padx=5, pady=5)
+        self.frame_button = Frame(self.gui_trans_input, bg='#F9E79F', highlightbackground='black', highlightthickness=1, padx=5, pady=5)
         self.frame_button.pack(fill='x', expand=True, padx=5, pady=3)
-        Button(self.frame_button, text="Enter", font=('arial', 10, 'bold'), width=11, activebackground='green', command=get_mat).grid(row=0, column=2, padx=14)
-        Button(self.frame_button, text="Back", font=('arial', 10, 'bold'), width=11, activebackground='green', command=self.back_to_dimensions).grid(row=0, column=1, padx=14)
-        Button(self.frame_button, text="Back to Menu", font=('arial', 10, 'bold'),activebackground='green', command=self.back_to_menu_from_input).grid(row=0, column=0, padx=14)
+        Button(self.frame_button, text="Nhập", font=('arial', 10, 'bold'), width=11, activebackground='green', command=get_mat).grid(row=0, column=2, padx=14)
+        Button(self.frame_button, text="Quay lại", font=('arial', 10, 'bold'), width=11, activebackground='green', command=self.back_to_dimensions).grid(row=0, column=1, padx=14)
+        Button(self.frame_button, text="Quay lại Menu", font=('arial', 10, 'bold'), activebackground='green', command=self.back_to_menu_from_input).grid(row=0, column=0, padx=14)
 
         self.gui_trans_input.protocol("WM_DELETE_WINDOW", menu.gui_menu.destroy)
         self.gui_trans_input.mainloop()
@@ -124,10 +145,10 @@ class Trans:
     def back_to_dimensions(self):
         self.gui_trans_input.destroy()
         self.__init__()
+
     def back_to_menu_from_input(self):
         self.gui_trans_input.destroy()
         menu.gui_menu.deiconify()
-
 
     def __init__(self):
         self.gui_trans_input = None
@@ -140,27 +161,28 @@ class Trans:
 
         menu.gui_menu.withdraw()
         self.gui_trans_menu = Toplevel()
-        self.gui_trans_menu.title("Transpose")
+        self.gui_trans_menu.title("Chuyển vị")
+        self.gui_trans_menu.geometry('270x500')
         self.gui_trans_menu.resizable(False, False)
 
-        self.frame_trans_menu = Frame(self.gui_trans_menu, highlightbackground='black', highlightthicknes=1, bg='#F9E79F', padx=5, pady=5)
+        self.frame_trans_menu = Frame(self.gui_trans_menu, highlightbackground='black', highlightthickness=1, bg='#F9E79F', padx=5, pady=5)
         self.frame_trans_menu.pack(fill=BOTH, expand=True, padx=5, pady=5)
 
-        Label(self.frame_trans_menu, text='Enter matrix dimensions:', font=('arial', 14, 'bold'), bg='#F9E79F').grid(row=0, column=0, columnspan=3, sticky='news', pady=10)
+        Label(self.frame_trans_menu, text='Nhập kích thước ma trận:', font=('arial', 14, 'bold'), bg='#F9E79F').grid(row=0, column=0, columnspan=3, sticky='news', pady=10)
 
-        # enter matrix dimensions:
+        # Nhập kích thước ma trận:
         self.rows = IntVar()
         self.rows.set(2)
-        OptionMenu(self.frame_trans_menu, self.rows, *range(1, 5)).grid(row=1, column=0, sticky='ew')
+        OptionMenu(self.frame_trans_menu, self.rows, *range(1, 21)).grid(row=1, column=0, sticky='ew')
 
         Label(self.frame_trans_menu, text='x', bg='#F9E79F', font=('arial', 14, 'bold')).grid(row=1, column=1)
 
         self.cols = IntVar()
         self.cols.set(2)
-        OptionMenu(self.frame_trans_menu, self.cols, *range(1, 5)).grid(row=1, column=2, sticky='ew')
-        Button(self.frame_trans_menu, text='Enter', font=('arial', 10, 'bold'), activebackground='green', padx=10, pady=5, command=self.input_matrix).grid(row=2, column=0, columnspan=3, pady=10)
-        # BACK TO MENU
-        Button(self.frame_trans_menu, text="Back to Menu", font=('arial', 10, 'bold'), activebackground='green', padx=10, pady=5, command=self.back_to_menu_from_dimensions).grid(row=3, column=0, columnspan=3)
+        OptionMenu(self.frame_trans_menu, self.cols, *range(1, 21)).grid(row=1, column=2, sticky='ew')
+        Button(self.frame_trans_menu, text='Nhập', font=('arial', 10, 'bold'), activebackground='green', padx=10, pady=5, command=self.input_matrix).grid(row=2, column=0, columnspan=3, pady=10)
+        # QUAY LẠI MENU
+        Button(self.frame_trans_menu, text="Quay lại Menu", font=('arial', 10, 'bold'), activebackground='green', padx=10, pady=5, command=self.back_to_menu_from_dimensions).grid(row=3, column=0, columnspan=3)
 
         self.gui_trans_menu.protocol("WM_DELETE_WINDOW", menu.gui_menu.destroy)
         self.gui_trans_menu.mainloop()
@@ -168,3 +190,7 @@ class Trans:
     def back_to_menu_from_dimensions(self):
         self.gui_trans_menu.destroy()
         menu.gui_menu.deiconify()
+
+if __name__ == "__main__":
+    trans_calculator = Trans()
+
