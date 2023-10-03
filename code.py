8ec8481fa0cfead3a65ba9400e15a3ff236ebab5
LeTriThanh2021603411 <144913112+LeTriThanh2021603411@users.noreply.github.com>
2023-10-03 10:20:20 +0700

    Update_Them_giao_dien.py

--- code.py
+++ code.py
@@ -1,68 +1,121 @@
+import sys
 import sympy as sp
 import matplotlib.pyplot as plt
+from PyQt6 import QtWidgets
 
-# Khai báo biến ký hiệu
-x = sp.symbols('x')
+class MathOperationsApp(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
 
-# Định nghĩa dãy số
-n = sp.symbols('n')
-sequence = 1/n
+        # Khai báo biến ký hiệu
+        self.x = sp.symbols('x')
 
-while True:
-    #them 1 print
-    print("Chọn tùy chọn:")
-    print("1. Tính đạo hàm")
-    print("2. Tính tích phân")
-    print("3. Giải phương trình")
-    print("4. Xác định tính hội tụ")
-    print("5. Thoát")
-    
-    choice = input("Nhập lựa chọn của bạn: ")
-    
-    if choice == '1':
-        expression = input("Nhập biểu thức (ví dụ: x**2 + 3*x - 1): ")
+        # Định nghĩa dãy số
+        self.n = sp.symbols('n')
+        self.sequence = 1 / self.n
+
+        # Create GUI elements
+        self.init_ui()
+
+    def init_ui(self):
+        # Create labels
+        self.label = QtWidgets.QLabel("Chọn tùy chọn:")
+
+        # Create input field
+        self.input_field = QtWidgets.QLineEdit()
+
+        # Create buttons
+        self.derivative_button = QtWidgets.QPushButton("Tính đạo hàm")
+        self.integral_button = QtWidgets.QPushButton("Tính tích phân")
+        self.equation_button = QtWidgets.QPushButton("Giải phương trình")
+        self.convergence_button = QtWidgets.QPushButton("Xác định tính hội tụ")
+        self.exit_button = QtWidgets.QPushButton("Thoát")
+
+        # Create a plot area
+        self.plot_widget = QtWidgets.QWidget()
+
+        # Set up layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.input_field)
+        layout.addWidget(self.derivative_button)
+        layout.addWidget(self.integral_button)
+        layout.addWidget(self.equation_button)
+        layout.addWidget(self.convergence_button)
+        layout.addWidget(self.exit_button)
+        layout.addWidget(self.plot_widget)
+
+        # Connect buttons to functions
+        self.derivative_button.clicked.connect(self.calculate_derivative)
+        self.integral_button.clicked.connect(self.calculate_integral)
+        self.equation_button.clicked.connect(self.solve_equation)
+        self.convergence_button.clicked.connect(self.check_convergence)
+        self.exit_button.clicked.connect(self.close_app)
+
+        self.setLayout(layout)
+
+    def calculate_derivative(self):
+        expression = self.input_field.text()
         try:
             f = sp.sympify(expression)
-            derivative = sp.diff(f, x)
-            print("Đạo hàm của", expression, "là:", derivative)
+            derivative = sp.diff(f, self.x)
+            self.show_result(f"Đạo hàm của {expression} là: {derivative}")
         except:
-            print("Biểu thức không hợp lệ.")
-    
-    elif choice == '2':
-        expression = input("Nhập biểu thức (ví dụ: x**2 + 3*x - 1): ")
+            self.show_result("Biểu thức không hợp lệ.")
+
+    def calculate_integral(self):
+        expression = self.input_field.text()
         try:
             f = sp.sympify(expression)
-            integral = sp.integrate(f, x)
-            print("Tích phân của", expression, "là:", integral)
+            integral = sp.integrate(f, self.x)
+            self.show_result(f"Tích phân của {expression} là: {integral}")
         except:
-            print("Biểu thức không hợp lệ.")
+            self.show_result("Biểu thức không hợp lệ.")
 
-    elif choice == '3':
-        equation = input("Nhập phương trình (ví dụ: x**2 - 4 = 0): ")
+    def solve_equation(self):
+        equation = self.input_field.text()
         try:
             equation = sp.Eq(sp.sympify(equation), 0)
-            solutions = sp.solve(equation, x)
-            print("Nghiệm của phương trình là:", solutions)
-        except :
-            print("Phương trình không hợp lệ.")
-    elif choice == '4':
-        # Xác định tính hội tụ của dãy số
-        limit = sp.limit(sequence, n, sp.oo)
+            solutions = sp.solve(equation, self.x)
+            self.show_result(f"Nghiệm của phương trình là: {solutions}")
+        except:
+            self.show_result("Phương trình không hợp lệ.")
 
-        print("Giới hạn của dãy số là:", limit)
+    def check_convergence(self):
+        limit = sp.limit(self.sequence, self.n, sp.oo)
+        self.show_result(f"Giới hạn của dãy số là: {limit}")
 
-        # Vẽ đồ thị của dãy số
-        n_values = range(1, 21)  # Dãy số từ n=1 đến n=20
-        sequence_values = [sp.N(sequence.subs(n, value)) for value in n_values]
-
+        # Create and display the sequence plot
+        n_values = range(1, 21)
+        sequence_values = [sp.N(self.sequence.subs(self.n, value)) for value in n_values]
+        plt.figure()
         plt.plot(n_values, sequence_values)
         plt.xlabel('n')
         plt.ylabel('Giá trị của dãy số')
         plt.title('Tính hội tụ của dãy số')
         plt.grid(True)
-        plt.show()
-    elif choice == '5':
-        break
+        plt.savefig('sequence_plot.png')
+        plt.close()
 
-    else:
-        print("Lựa chọn không hợp lệ. Vui lòng chọn lại.")
+        # Display the plot in the GUI
+        plot_image = QtGui.QImage('sequence_plot.png')
+        plot_pixmap = QtGui.QPixmap.fromImage(plot_image)
+        self.plot_widget.setPixmap(plot_pixmap)
+
+    def show_result(self, result):
+        msg_box = QtWidgets.QMessageBox()
+        msg_box.setIcon(QtWidgets.QMessageBox.Icon.Information)
+        msg_box.setText(result)
+        msg_box.setWindowTitle("Kết quả")
+        msg_box.exec()
+
+    def close_app(self):
+        self.close()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MathOperationsApp()
+    window.setWindowTitle("Math Operations")
+    window.show()
+    sys.exit(app.exec())
